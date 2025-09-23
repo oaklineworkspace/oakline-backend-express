95147925526ccd1e75b2b4395aff667cd8e1f43b
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-23 11:00:43 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import { enrollUser, verifyIdentity } from '../controllers/users/userController.js';
-import { transferFunds, getTransactionHistory } from '../controllers/users/accounts.js';
-import { verifyToken } from '../lib/middleware/authMiddleware.js';
+import { requestEnrollment, completeEnrollment } from '../controllers/users/userController.js';
 
 const router = express.Router();
 
 // Public routes
-router.post('/verify-identity', verifyIdentity);
-router.post('/enroll', enrollUser);
-
-// Protected routes
-router.post('/transfer', verifyToken, transferFunds);
-router.get('/:accountId/transactions', verifyToken, getTransactionHistory);
+router.post('/request-enroll', requestEnrollment);
+router.post('/complete-enroll', completeEnrollment);
 
 export default router;
