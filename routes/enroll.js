317e5318a4713736f4e09c2f413210df8edb99bc
oaklineworkspace <oaklineworkspace@gmail.com>
2025-09-23 12:36:29 -0700

    Delete routes/enroll.js

--- routes/enroll.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { requestEnrollment, completeEnrollment } from '../controllers/enrollController.js';
-
-const router = express.Router();
-
-router.post('/request', requestEnrollment);
-router.post('/complete', completeEnrollment);
-
-export default router;
